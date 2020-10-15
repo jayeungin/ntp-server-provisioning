1afaee94e021188342c04bcf3d9e35aef4608c3e
jayeungin <jasonyeung2694@gmail.com>
2020-10-15 13:09:31 -0400

    adding shell intall script for salt

--- provision-ntp.tf
+++ provision-ntp.tf
@@ -7,20 +7,46 @@ terraform {
   }
 }
 
-// Defining cloud provider, profile and region
+# Defining cloud provider, profile and region
 provider "aws" {
-  // If you don't want to use default delete the line, then use next two lines calling access_key and secret_key
-  // "default" uses the AWS credentials stored in your AWS Config File when you initially set up AWS CLI with "aws config"
+
+  # "default" uses the AWS credentials stored in your AWS Config File through AWS CLI via "aws config"; If you don't want to use default delete the line, then add two lines calling access_key and secret_key
   profile = "default"
   region  = "us-east-2"
 }
 
-// Creating an aws ec2 instance
-// Specify desired ami and instance_type
-resource "aws_instance" "ntp_server" {
-  // CentOS 7 image needs to match region, see https://wiki.centos.org/Cloud/AWS
-  ami           = "ami-0a75b786d9a7f8144"
+# Creating an aws ec2 instance
+# Specify desired ami and instance_type
+resource "aws_instance" "master_ntp_server" {
+
+  # CentOS 7 image needs to match region, see https://wiki.centos.org/Cloud/AWS
+  ami = "ami-0a75b786d9a7f8144"
   instance_type = "t2.micro"
-  // Use AWS EC2 Key Pairs to create key pair, if you don't do this you won't be able to ssh/putty into your EC2
+
+  # Use AWS EC2 Key Pairs to create key pair, if you don't do this you won't be able to ssh/putty into your EC2
   key_name = "ntp-server"
+
+  # Pass in scripts to be executed in the first boot of the instance
+  user_data = "${file("Saltstack/install_salt.sh")}"
+
+  tags = {
+  Saltstack = "master"
+  }
+}
+
+resource "aws_instance" "minion_ntp_server" {
+
+  # CentOS 7 image needs to match region, see https://wiki.centos.org/Cloud/AWS
+  ami = "ami-0a75b786d9a7f8144"
+  instance_type = "t2.micro"
+
+  # Use AWS EC2 Key Pairs to create key pair, if you don't do this you won't be able to ssh/putty into your EC2
+  key_name = "ntp-server"
+
+  # Pass in scripts to be executed in the first boot of the instance
+  user_data = "${file("Saltstack/install_salt.sh")}"
+
+  tags = {
+  Saltstack = "minion"
+  }
 }
