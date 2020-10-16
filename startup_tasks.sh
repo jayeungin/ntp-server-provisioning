c19a7ed32ae22b1b2989154be26fffcb15c0c0aa
jayeungin <jasonyeung2694@gmail.com>
2020-10-15 21:41:38 -0400

    adding commands for salt

--- startup_tasks.sh
+++ startup_tasks.sh
@@ -1,9 +1,14 @@
 sudo su
 yum -y update
-# Bootstrap a masterless salt-minion
+# Bootstrapping a masterless salt-minion
 curl -L https://bootstrap.saltstack.com -o bootstrap_salt.sh
 sh bootstrap_salt.sh
-yum install vim
-yum install git
+# Installing VIM
+yum -y install vim
+# Installing git
+yum -y install git
 cd ..
 git clone https://github.com/jayeungin/ntp-server-provisioning.git
+# Copying Salt files
+cp Saltstack/ntp.conf Saltstack/ntp.sls Saltstack/top.sls ../srv/salt
+salt-call --local state.apply -l debug
